62a959006afdf99cd73c858f2c5b77273b0e0124
Olivier Moliner <olivier.moliner@sonymobile.com>
2019-02-17 02:30:46 +0100

    doc: Document lic_0 function

--- decide/lic.py
+++ decide/lic.py
@@ -2,6 +2,18 @@ import math
 
 
 def lic_0(points, parameters):
+    """ Checks whether Launch Interceptor Condition 0 is met
+
+    Determines whether there exist two consecutive points that are a distance
+    greater than LENGTH1 apart
+
+    Args:
+        points (list): List of coordinates of data points
+        parameters (dict): Parameters for the LICs
+
+    Returns
+        bool: True if the condition is met
+    """
     for i in range(len(points) - 1):
         point1 = [points[i][0], points[i][1]]
         point2 = [points[i + 1][0], points[i + 1][1]]
