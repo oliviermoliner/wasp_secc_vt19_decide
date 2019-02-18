be9cd427301fce9076ac1d35bfeca837db80d400
Olivier Moliner <olivier.moliner@sonymobile.com>
2019-02-18 01:49:58 +0100

    test: Add test cases for the lic_0 function

--- test/test_lic.py
+++ test/test_lic.py
@@ -1,3 +1,5 @@
+import pytest
+
 from decide import lic
 
 
@@ -9,3 +11,33 @@ def test_lic0_met():
 def test_lic0_not_met():
     """LIC 0 should not be met when no consecutive points are more than LENGTH1 apart"""
     assert lic.lic_0([[0, 0], [1, 1], [2, 2]], {"length1": 2}) is False
+
+
+@pytest.mark.parametrize(
+    "points,parameters",
+    [
+        ([[0, 0], [1, 0], [0.5, 0.86602539]], {"radius1": 0.5}),
+        ([[0, 0], [1, 0], [0.5, 10]], {"radius1": 5}),
+    ],
+)
+def test_lic1_met(points, parameters):
+    """
+    LIC 1 should be met when three consecutive points cannot all be contained within
+    or on a circle of radius RADIUS1
+    """
+    assert lic.lic_1(points, parameters) is True
+
+
+@pytest.mark.parametrize(
+    "points,parameters",
+    [
+        ([[0, 0], [0, 4], [2, 2]], {"radius1": 2}),
+        ([[0, 0], [1, 0], [10, 2]], {"radius1": 24}),
+    ],
+)
+def test_lic1_not_met(points, parameters):
+    """
+    LIC 1 should not be met when all sets of three consecutive points can becontained
+    within or on a circle of radius RADIUS1
+    """
+    assert lic.lic_1(points, parameters) is False
